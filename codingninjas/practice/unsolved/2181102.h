d407ce18344b062acad8bb6b4e2d3ceebbc83be5
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-19 09:08:10 +0530

    Create 2181102.h

---
+++ codingninjas/practice/unsolved/2181102.h
@@ -0,0 +1,24 @@
+/*
+2181102
+bhatoresiddharth@gmail.com
+sidb95
+19 May 2024
+*/
+#include <math.h>
+
+long reverseBits(long n) {
+    long m = 0;
+    m = n;
+    int count = 0;
+    while (m != 0) {
+        m /= 2;
+        count++;
+    }
+    long y = 0;
+    long retAnswer = 0;
+    for (int i = n; i != 0; i /= 2) {
+        retAnswer += (i % 2) * pow(2, count-1);
+        count--;
+    }
+    return retAnswer;
+}
