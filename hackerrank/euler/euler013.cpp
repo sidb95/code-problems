0d8def44270189a98d0335a3a3b06a9e7befe7de
= <=>
2024-05-22 18:20:53 +0530

    Update euler013.cpp
    Success

--- hackerrank/euler/euler013.cpp
+++ hackerrank/euler/euler013.cpp
@@ -5,14 +5,55 @@ sidb95
 22 May 2022
 */
 
-string calcFirstTen(int N, string s) {
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+int K = 50;
+
+string calcFirstTen(vector<string>& V1, int N) {
+    string answer = "";
+    string retAnswer = "";
+    int unitsDigit;
+    //
+    int sum, carry = 0;
+    // runs K times, i = K-1 to 0
+    for (int i = K - 1; i >= 0; i--) {
+        sum = 0;
+        // at position i
+        for (int j = 0; j < N; j++) {
+            sum += (V1[j][i] - '0');
+        }
+        // unit place digit calculation
+        unitsDigit = ((sum + carry) % 10);
+        carry = ((sum + carry) / 10);
+        answer = to_string(unitsDigit) + answer;
+    }
+    // adding carry forward to the answer
+    while (carry != 0) {
+        unitsDigit = (carry % 10);
+        carry /= 10;
+        answer = to_string(unitsDigit) + answer;
+    }
+    // making retAnswer (first 10 digits of answer)
+    for (int i = 0; i < 10; i++) {
+        retAnswer += answer[i];
+    }
+    //
+    return retAnswer;
 }
 
 int main() {
-    string s;
-    short int N;
-    cin >> N >> s;
-    cout << calcFirstTen(N, s) << endl;
+    vector<string> V1;
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+        V1.push_back(s);
+    }
+    cout << calcFirstTen(V1, N) << endl;
     return 0;
 }
