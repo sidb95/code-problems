2141964a329763ce6ee86233f18a3ae5b5f43eb6
= <=>
2024-05-22 17:28:22 +0530

    Update euler013.cpp

--- hackerrank/euler/euler013.cpp
+++ hackerrank/euler/euler013.cpp
@@ -4,3 +4,15 @@ bhatoresiddharth@gmail.com
 sidb95
 22 May 2022
 */
+
+string calcFirstTen(int N, string s) {
+
+}
+
+int main() {
+    string s;
+    short int N;
+    cin >> N >> s;
+    cout << calcFirstTen(N, s) << endl;
+    return 0;
+}
