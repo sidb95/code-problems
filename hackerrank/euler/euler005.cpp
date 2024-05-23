a0376f7531013d870ba202809fdf2b16061f40bb
= <=>
2024-05-23 15:01:44 +0530

    update euler005.cpp
    Success

--- hackerrank/euler/euler005.cpp
+++ hackerrank/euler/euler005.cpp
@@ -2,7 +2,7 @@
 euler005
 bhatoresiddharth@gmail.com
 sidb95
-22 May 2024
+22, 23 May 2024
 */
 
 #include <iostream>
@@ -31,28 +31,29 @@ long long int calcAnswer(long long int n);
  final calculation
 */
 //
-long long int finalCalc(long long int n, set <int>& placed, 
-long long int product, set<int>& primes, set<int>& nonprimes);
+long long int finalCalc(long long int n, set <long long int>& placed, 
+long long int product, set<long long int>& primes, 
+set<long long int>& nonprimes);
 //
 /*
 check for product divisibility
 */
 //
-bool checkAuxHelper(long long int product, set<int>& placed);
+bool checkAuxHelper(long long int product, set<long long int>& placed);
 //
 /*
 check for divisibility by nonprime np1
 */
 //
-void checkDivisible(long long int product, set <int>& placed, 
+void checkDivisible(long long int product, set <long long int>& placed, 
 long long int k);
 //
 /*
  helper function for ```calcAnswer```
 */
 //
-long long int calcAnswerAux(set <int>& primes, set <int>& nonprimes, 
-long long int n);
+long long int calcAnswerAux(set <long long int>& primes, 
+set <long long int>& nonprimes, long long int n);
 //
 bool isPrime(long long int num, long long int limit) {
     bool retAnswer = true;
@@ -89,10 +90,10 @@ bool isPrime(long long int num, long long int limit) {
     return retAnswer;
 }
 
-bool checkAuxHelper(long long int product, set<int>& placed) {
-    set <int>::iterator itr1;
+bool checkAuxHelper(long long int product, set<long long int>& nonprimes) {
+    set <long long int>::iterator itr1;
     long long int k;
-    for (itr1 = placed.begin(); itr1 != placed.end(); itr1++) {
+    for (itr1 = nonprimes.begin(); itr1 != nonprimes.end(); itr1++) {
         k = (*itr1);
         if ((product % k) != 0) {
             return false;
@@ -101,39 +102,30 @@ bool checkAuxHelper(long long int product, set<int>& placed) {
     return true;
 }
 
-void checkDivisible(long long int product, set <int>& placed, long long int k) {
+void checkDivisible(long long int product, set <long long int>& placed, 
+long long int k) {
     if ((product % k) != 0) {
         placed.insert(k);
     }
     return;
 }
 
-long long int finalCalc(long long int n, set <int>& placed, 
-long long int product, set<int>& primes, set <int>& nonprimes) {
-    int len1 = placed.size();
-    if (len1 == 0) {
-        return product;
-    }
-    set <int>::iterator itr1;
-    bool propn1;
-    long long int k;
-    long long int answer;
+long long int finalCalc(long long int n, set <long long int>& placed, 
+long long int product, set<long long int>& primes, 
+set <long long int>& nonprimes) {
+    long long int k = 2;
+    long long int answer = product;
     //
-    for (itr1 = placed.begin(); itr1 != placed.end(); itr1++) {
-        k = (*itr1);
-        answer = product * k;
-        propn1 = checkAuxHelper(answer, placed);
-        if (propn1) {
-            return answer;
-        }
+    while (!checkAuxHelper(answer * k, nonprimes)) {
+        k++;
     }
-    return answer;
+    return (answer * k);
 }
 
-long long int calcAnswerAux(set <int>& primes, set <int>& nonprimes, 
-long long int n) {
-    set <int>::iterator itr1; // primes iterator
-    set <int>::iterator itr2; // nonprimes iterator
+long long int calcAnswerAux(set <long long int>& primes, 
+set <long long int>& nonprimes, long long int n) {
+    set <long long int>::iterator itr1; // primes iterator
+    set <long long int>::iterator itr2; // nonprimes iterator
     //
     long long int product = 1;
     // calc product to product of all primes
@@ -142,7 +134,7 @@ long long int n) {
     }
     //
     long long int k;
-    set <int> placed;
+    set <long long int> placed;
     //
     for (itr2 = nonprimes.begin(); itr2 != nonprimes.end(); itr2++) {
         k = (*itr2);
@@ -156,16 +148,16 @@ long long int calcAnswer(long long int n) {
         return 1;
     }
     //
-    set <int> primes;
-    set <int> nonprimes;
+    set <long long int> primes;
+    set <long long int> nonprimes;
     // set primes
-    for (int i = 2; i <= n; i++) {
+    for (long long int i = 2; i <= n; i++) {
         if (isPrime(i, pow(i, 0.5))) {
             primes.insert(i);
         }
     }
     // set non primes
-    for (int i = 2; i <= n; i++) {
+    for (long long int i = 2; i <= n; i++) {
         if (primes.find(i) == primes.end()) {
             nonprimes.insert(i);
         }
