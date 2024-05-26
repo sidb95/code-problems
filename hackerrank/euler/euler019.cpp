4b41a079bb2948449e70d26dba7b084fa662dba5
= <=>
2024-05-26 12:45:36 +0530

    Update euler019.cpp, create euler020.cpp

--- hackerrank/euler/euler019.cpp
+++ hackerrank/euler/euler019.cpp
@@ -10,13 +10,13 @@ sidb95
 
 using namespace std;
 
-vector <int> SET_DAYS = {
+vector <long long int> SET_DAYS = {
     31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
 };
 
 // checks if the year is leap year
-bool isLeapYear(long long int Y) {
-    int febDays;
+bool isLeapYear(long long int year) {
+    int febDays = 28;
     if ((year % 100) == 0) {
         if ((year % 400) == 0) {
             febDays = 29;
@@ -59,14 +59,14 @@ int calcDays2(long long int Y, int M, int D) {
             }
         }
     }
-    return answer += (D - 1);
+    return answer += (D);
 }
 
 // return (0, 6), set member for the given date
 int dayThisDate(long long int Y, int M, int D) {
     long long int days = 0;
     for (long long int year = 1900; year < Y; year += 1) {
-        days += calcDays2(year, 12, 31)
+        days += calcDays2(year, 12, 31);
     }
     days += calcDays2(Y, M, D);
     return (days + 1) % 7;
@@ -75,7 +75,7 @@ int dayThisDate(long long int Y, int M, int D) {
 // answer to the question taking two dates
 int noMonthlySunday(long long int Y1, int M1, int D1, long long int Y2, int M2, 
 int D2) {
-    answer = 0;
+    int answer = 0;
     for (int year = Y1 + 1; year < Y2; year += 1) {
         for (int month = 1; month <= 12; month += 1) {
             if (dayThisDate(year, month, 1) == 0) {
@@ -83,12 +83,12 @@ int D2) {
             }
         }
     }
-    for (int i = M1; i <= 12; i++) {
+    for (int i = ((M1 == 1)? 2 : M1); i <= 12; i += 1) {
         if (dayThisDate(Y1, i, 1) == 0) {
             answer += 1;
         }
     }
-    for (int i = 1; i <= M2; i++) {
+    for (int i = ((M2 == 1)? 2 : M2); i <= M2; i += 1) {
         if (dayThisDate(Y2, i, 1) == 0) {
             answer += 1;
         }
---
+++ hackerrank/euler/euler020.cpp
@@ -0,0 +1,17 @@
+/*
+euler020
+bhatoresiddharth@gmail.com
+sidb95
+26 May 2024
+*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    return 0;
+}
