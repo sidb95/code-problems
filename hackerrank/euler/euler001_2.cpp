7a475307bec54fdc2580690d17fb4b591c0e5c0a
= <=>
2024-05-26 17:42:09 +0530

    Update euler001_2.cpp

--- hackerrank/euler/euler001_2.cpp
+++ hackerrank/euler/euler001_2.cpp
@@ -9,11 +9,12 @@ sidb95
 #include <set>
 #include <math.h>
 #include <algorithm>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-long long int SET_LIMIT;
-long long int SET_LIMIT_1 = 1000000;
+map < int, vector <int > > M1;
 
 // sum of digits of N
 int sumOfDigits(long long int N) {
@@ -30,151 +31,121 @@ bool divisibilityCheck3(long long int N) {
     int n = sumOfDigits(N);
     return (n % 3) == 0;
 }
-
+// if num is divisible by 5
 bool divisibilityCheck5(long long int N) {
     return ((N % 10) == 0) || ((N % 10) == 5);
 }
-
 //
-pair<int, int> assignerFunc(long long int n, long long int lastNum, long long int maxNum) {
-    pair<int, int> retAnswer;
-    int posi, posj;
-    // maxNum is greater than n, case 1
-    if (maxNum > n) {
-        // posi calc, case 1
-        if (divisibilityCheck3(n)) {
-            posi = n;
+long long int sumMultiplesAux1A(long long int n, long long int sum, 
+bool FLAG) {
+    for (long long int l2 = 3; l2 < n; l2 += 3) {
+        if (FLAG) {
+            M1[3].push_back(l2);
         }
-        else if ((divisibilityCheck3(n + 1))) {
-            posi = n + 1;
-        }
-        else if ((divisibilityCheck3(n + 2))) {
-            posi = n + 2;
-        }
-        else {
-            posi = 3;
-        }
-        // posj calc, case 1
-        if (divisibilityCheck5(n)) {
-            posj = n;
-        }
-        else if (divisibilityCheck5(n + 1)) {
-            posj = n + 1;
-        }
-        else if (divisibilityCheck5(n + 2)) {
-            posj = n + 2;
-        }
-        else if (divisibilityCheck5(n + 3)) {
-            posj = n + 3;
-        }
-        else if (divisibilityCheck5(n + 4) ) {
-            posj = n + 4;
-        }
-        else {
-            posj = 5;
-        }
+        sum += l2;
     }
-    else if (maxNum == n){
-        if (lastNum == -1) {
-            lastNum = 1;
-        }
-        if (divisibilityCheck3(lastNum)) {
-            posi = lastNum;
-        }
-        else if ((divisibilityCheck3(lastNum + 1)) ) {
-            posi = lastNum + 1;
-        }
-        else if ((divisibilityCheck3(lastNum + 2)) ) {
-            posi = lastNum + 2;
-        }
-        else {
-            posi = 3;
-        }
-        if (divisibilityCheck5(lastNum) ) {
-            posj = lastNum;
-        }
-        else if ((divisibilityCheck5(lastNum + 1)) ) {
-            posj = lastNum + 1;
-        }
-        else if ((divisibilityCheck5(lastNum + 2)) ) {
-            posj = lastNum + 2;
-        }
-        else if ((divisibilityCheck5(lastNum + 3)) ) {
-            posj = lastNum + 3;
-        }
-        else if ((divisibilityCheck5(lastNum + 4))) {
-            posj = lastNum + 4;
-        }
-        else {
-            posj = 5;
-        }
-    }
-    retAnswer = make_pair(posi, posj);
-    return retAnswer;
+    return sum;
 }
-
 //
-long long int sumMultiples(long long int n, set <int>& VIS, long long int maxNum, long long int lastNum) {
+long long int sumMultiplesAux1B(long long int n, long long int sum, 
+bool FLAG) {
+    for (long long int l2 = 5; l2 < n; l2 += 5) {
+        if (!divisibilityCheck3(l2)) {
+            if (FLAG) {
+                M1[5].push_back(l2);
+            }
+            sum += l2;
+        }
+    }
+    return sum;
+}
+//
+long long int sumMultiplesAux2(long long int n, long long int maxNum, 
+long long int sum) {
+    int m = M1[3].size();
+    //
+    bool propn1 = true;
+    propn1 = (m < maxNum);
+    //
+    for (int i = 0; i < (propn1? m : maxNum); i++) {
+        sum += M1[3][i];
+    }
+    int q = M1[5].size();
+    //
+    bool propn2 = true;
+    propn2 = (q < maxNum);
+    //
+    for (int i = 0; i < (propn2? q : maxNum); i++) {
+        sum += M1[5][i];
+    }
+    // if condition holds or not
+    if (propn1) {
+        long long int posi = ((divisibilityCheck3(m)? m : 
+        divisibilityCheck3(m + 1)? (m + 1) : (m + 2)));
+        for (int i = posi; i < n; i += 3) {
+            sum += i;
+            M1[3].push_back(i);
+        }
+    }
+    // if condition holds or not
+    if (propn2) {
+        long long int posj = ((divisibilityCheck5(q)))? q : 
+        (divisibilityCheck5((q + 1))? (q + 1) : 
+        (divisibilityCheck5(q + 2))? (q + 2) :
+        (divisibilityCheck5(q + 3))? (q + 3) : (q + 4));
+        for (int i = posj; i < n; i += 5) {
+            if (!divisibilityCheck3(i)) {
+                sum += i;
+            }
+        }
+    }
+    return sum;
+}
+//
+long long int sumMultiplesAux3(long long int n, long long int maxNum, 
+long long int sum) {
+    int m = M1[3].size();
+    if (m > maxNum) {
+        for (int i = 0; M1[3][i] < n; i += 1) {
+            sum += M1[3][i];
+        }    
+    }
+    int q = M1[5].size();
+    for (int i = 0; M1[5][i] < n; i += 1) {
+        sum += M1[5][i];
+    }
+    return sum;
+}
+//
+long long int sumMultiplesAux(long long int n, long long lastNum, 
+long long int maxNum, long long int sum) {
+    //
+    if (lastNum == -1) {
+        sum = sumMultiplesAux1A(n, sum, true);
+        sum = sumMultiplesAux1B(n, sum, true);
+    }
+    else if (lastNum < maxNum) {
+        sum = sumMultiplesAux2(n, maxNum, sum);
+    }
+    else if (lastNum == maxNum) {
+        sum = sumMultiplesAux3(n, maxNum, sum);
+    }
+    else {
+        sum = 0;
+    }
+    return sum;
+}
+//
+long long int sumMultiples(long long int n, long long int lastNum, 
+long long int maxNum) {
     //
     long long int sum = 0;
     // edge cases
     if((n == 1) || (n == 2)) {
-        return 0;
-    }
-    // looping till SET_LIMIT
-    if (n < SET_LIMIT_1) {
-        for (int i = 3; i < n; i++) {
-            if (divisibilityCheck3(i) || divisibilityCheck5(i)) {
-                sum += i;
-                VIS.insert(i);        
-            }
-        }
         return sum;
     }
-
-    //
-    int posi, posj;
-    // calc posi posj
-    pair<int, int> p;
-    p = assignerFunc(n, lastNum, maxNum);
-    //
-    posi = p.first;
-    posj = p.second;
-    //
-    int m = VIS.size();
-    int k;
-    //
-    set <int>::iterator itrVis;
-    // summing up the largest non-conflicting summation
-    for (itrVis = VIS.begin(); itrVis != VIS.end(); itrVis++) {
-        k = (*itrVis);
-        if (k < n) {
-            sum += k;
-        }
-        else {
-            return sum;
-        }
-    }
-    // summing up (if not returned), multiples of a,
-    for (int i = posi; i < n; i += 3) {
-        sum += i;
-        // exclude those i that are large in size 
-        if (SET_LIMIT >= m) {
-            m = VIS.size();
-            VIS.insert(i);
-        }
-    }
-    // summing up the new multiples of ```b < n```
-    for (int j = posj; j < n; j += 5) {
-        if (!divisibilityCheck3(j)) {
-            sum += j;
-            // exclude those j that are large in size 
-            if (SET_LIMIT >= m) {
-                m = VIS.size();
-                VIS.insert(j);
-            }
-        }
-    }
+    // first time in this function
+    sum = sumMultiplesAux(n, lastNum, maxNum, sum);
     //
     return sum;
 }
@@ -182,16 +153,16 @@ long long int sumMultiples(long long int n, set <int>& VIS, long long int maxNum
 int main(){
     int t;
     cin >> t;
-    set <int> VIS;
-    SET_LIMIT = VIS.max_size();
-    long long int maxNum = -1, lastNum;
+    long long int lastNum, maxNum = -1;
+    // testcases
     for(int a0 = 0; a0 < t; a0++){
         int n;
         cin >> n;
         lastNum = maxNum;
         long long int x = n;
         maxNum = max(maxNum, x);
-        cout << sumMultiples(x, VIS, maxNum, lastNum) << endl;
+        // print function
+        cout << sumMultiples(x, lastNum, maxNum) << endl;
     }
     return 0;
 }
