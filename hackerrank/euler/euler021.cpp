2a061c9205a62338db6474eabef4a84683a59391
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-26 21:42:29 +0530

    Update euler021.cpp

--- hackerrank/euler/euler021.cpp
+++ hackerrank/euler/euler021.cpp
@@ -12,6 +12,8 @@ May 26 2024
 
 using namespace std;
 
+long long int SET_LIMIT = 10000;
+
 long long int sumAmicablePairsAux(long long int N) {
     long long int sum = 0;
     for (long long int i = 1; i < N; i += 1) {
@@ -23,41 +25,64 @@ long long int sumAmicablePairsAux(long long int N) {
 }
 
 long long int sumAmicablePairs(long long int N, set <long long int>& S1, 
-long long int sum) {
+vector <long long int>& summation, long long int sum) {
     //
-    if (N == 1) {
+    if (N == 0) {
         return sum;
     }
-    else {
+    //
+    if (N < SET_LIMIT) {
+        long long int sumA = summation[N];
         //
-        long long int sumA = sumAmicablePairsAux(N);
-        //
-        for (long long int i = 1; i <= N - 1; i++) {
-            long long int sumB = sumAmicablePairsAux(i);
-            if (sumA == sumB) {
-                if (S1.find(sumA) == S1.end()) {
-                    S1.insert(sumA);
-                    sum += N;
-                }
-                if (S1.find(sumB) == S1.end()) {
-                    S1.insert(sumB);
-                    sum += i;
-                }
+        if (summation[sumA] == N) {
+            if (S1.find(sumA) == S1.end()) {
+                sum += sumA;
+                S1.insert(sum);
+            }
+            if (S1.find(N) == S1.end()) {
+                sum += N;
+                S1.insert(N);
             }
         }
-        return (sum) + sumAmicablePairs(N - 1, S1, sum);
+        //
+        return sumAmicablePairs(N - 1, S1, summation, sum);
     }
-    return sum;
+    else {
+        long long int sum1 = sumAmicablePairsAux(N);
+        if (sumAmicablePairsAux(sum1) == N) {
+            if (S1.find(sum1) == S1.end()) {
+                sum += sum1;
+                S1.insert(sum1);
+            }
+            if (S1.find(N) == S1.end()) {
+                sum += N;
+                S1.insert(N);
+            }
+        }
+        summation.push_back(sum1);
+        return sumAmicablePairs(N-1, S1, summation, sum);
+    }
 }
 
 int main() {
     int t, T;
     cin >> T;
+    vector <long long int> summation = {0};
+    for (long long int i = 1; i <= SET_LIMIT; i += 1) {
+        long long int K = (i)/2;
+        long long int sum = 1;
+        for (long long int j = 2; j <= K; j += 1) {
+            if ((i % j) == 0) {
+                sum += j;
+            }
+        }
+        summation.push_back(sum);
+    }
     for (t = 0; t < T; t += 1) {
         long long int N;
         cin >> N;
         set <long long int> S1;
-        cout << sumAmicablePairs(N, S1, 0) << endl;
+        cout << sumAmicablePairs(N, S1, summation, 0) << endl;
     }
     return 0;
 }
