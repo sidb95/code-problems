861cd0a8ce71eb984b38c7bd1a408690d145f147
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-27 10:46:41 +0530

    Add euler024.cpp

---
+++ hackerrank/euler/euler024.cpp
@@ -0,0 +1,28 @@
+/*
+euler024
+bhatoresiddharth@gmail.com
+sidb95
+27 May 2024
+*/
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+string calcAnswer(long long int N) {
+
+}
+
+int main() {
+    int t, T;
+    cin >> T;
+    for (t = 0; t < T; t++) {
+        long long int N;
+        cin >> N;
+        cout << calcAnswer(N) << endl;
+    }
+    return 0;
+}
