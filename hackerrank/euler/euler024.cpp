0a3f7b625330d636611b1d992772896694e434f2
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-27 11:43:00 +0530

    Update euler024.cpp

--- hackerrank/euler/euler024.cpp
+++ hackerrank/euler/euler024.cpp
@@ -11,9 +11,69 @@ sidb95
 
 using namespace std;
 
+string S = "abcdefghijklm";
+
+long long int factorial(int n) {
+    int answer = 1;
+    for (int i = 2; i <= n; i += 1) {
+        answer *= i;
+    }
+    return answer;
+}
+
+string swap(string s, int i, int j) {
+    char c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+    return s;
+}
+
+string calcAnswerAux(string s, int i, int j) {
+    s = swap(s, i, j);
+    return s;
+}
 
 string calcAnswer(long long int N) {
-
+    int m = S.size();
+    long long int num1 = factorial(m);
+    string s = S;
+    int count = 0;
+    //
+    bool FLAG = true;
+    //
+    int j, k = 0;
+    int i = 0;
+    //
+    while (FLAG) {
+        for (j = i; j < m - 1; j += 1) {
+            //
+            count++;
+            if (count == N) {
+                FLAG = false;
+                break;
+            }
+            //
+            s = calcAnswerAux(s, j, j + 1);
+        }
+        //
+        if (k == (m - 1)) {
+            k = 0;
+            i = 0;
+        }
+        //
+        else if (FLAG) {
+            s = S;
+        }
+        //
+        if (i == m - 1) {
+            i = k + 1;
+            k += 1;
+        }
+        else {
+            i += 1;
+        }
+        //
+    }
 }
 
 int main() {
