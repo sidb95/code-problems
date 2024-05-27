65baf0aefff986617cf126e8ea578a938e2a1c3f
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-27 16:47:33 +0530

    Update euler024.cpp
    tle

--- hackerrank/euler/euler024.cpp
+++ hackerrank/euler/euler024.cpp
@@ -16,8 +16,8 @@ using namespace std;
 string S = "abcdefghijklm";
 string Y = "mlkjihgfedcba";
 
-unordered_map< long int, string > um1;
-long int SET_LIMIT = um1.max_size();
+unordered_map< int, string > um1;
+long int SET_LIMIT;
 
 string swap(string s, int i, int j) {
     char c = s[i];
@@ -27,12 +27,14 @@ string swap(string s, int i, int j) {
 }
 
 // one greater lexicographical
-string greaterLexicographical1(string s, int m, long int N) {
+string greaterLexicographical1(string s, int m, long int N, 
+unordered_map <int, string>& um1) {
     if (s == Y) {
         return s;
     }
     else {
         long int count = 1;
+        int n;
         while (count != N) {
             for (int i = m - 1; i >= 1; i -= 1) {
                 while (s[i] < s[i - 1]) {
@@ -40,18 +42,25 @@ string greaterLexicographical1(string s, int m, long int N) {
                 }
                 s = swap(s, i, i - 1);
                 count += 1;
-                um1[count] = s;
+                n = um1.size();
+                if (n < SET_LIMIT) {
+                    um1[count] = s;
+                }
                 if (count == N) {
                     break;
                 }
             }
+        }
+        n = um1.size();
+        if (n < SET_LIMIT) {
+            um1[N] = s;
         }
         return s;
     }
     return s;
 }
 
-string calcAnswer(long int N) {
+string calcAnswer(long int N, unordered_map< int, string >& um1) {
     string s = S;
     if (N == 1) {
         um1[1] = s;
@@ -64,7 +73,7 @@ string calcAnswer(long int N) {
         int m = s.size();
         // setting s
         // taking one lexicographical greater than s
-        s = greaterLexicographical1(s, m, N);
+        s = greaterLexicographical1(s, m, N, um1);
     }
     return s;
 }
@@ -75,8 +84,9 @@ int main() {
     for (t = 0; t < T; t += 1) {
         long int N;
         cin >> N;
+        SET_LIMIT = um1.max_size();
         // prints the answer
-        cout << calcAnswer(N) << endl;
+        cout << calcAnswer(N, um1) << endl;
     }
     return 0;
 }
