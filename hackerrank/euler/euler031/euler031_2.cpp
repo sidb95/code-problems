14ffa01a055c72ae730fc63a692dc077a0fd1010
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-29 22:57:34 +0530

    Update euler031_2.cpp

--- hackerrank/euler/euler031/euler031_2.cpp
+++ hackerrank/euler/euler031/euler031_2.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 class SolutionM1 {
-
+private:
+    set <int> S = {1, 2, 5, 10, 20, 50, 100, 200};
 protected:
     set <int> S1 = {1, 2, 5, 10, 20, 50, 100, 200};
     set < int > Sa1 = {};
@@ -26,95 +27,25 @@ public:
         return (S1.find(N) == S1.end());
     }
     //
-    long long int calcAnswerAuxC1(int k, int N, set <int> Sb1) {
-        //
-        int n1 = Sa1.size();
-        //
+    //
+    long long int calcAnswerAuxA1(int N) {
         long long int retAnswer = 0;
-        set < int >::iterator itr;
-        //
-        int i = 1;
-        int num1 = (N / k);
-        int numA1;
-        //
-        while (k < N) {
-            numA1 = N - (num1 * i);
-            //
-            itr = Sa1.find(numA1);
-            if (itr == Sa1.end()) {
-                retAnswer += calcAnswer(numA1, 0);
-                //
-                if (MAX_LIMIT > (n1)) {
-                    Sa1.insert(numA1);
-                }
+        set <int>::iterator itr;
+        bool FLAG = true;
+        if (calcAnswerAuxB1(N)) {
+            retAnswer += 1;
+        }
+        for (int pence = N - 1; pence >= 1; pence -= 1) {
+            if (Sa1.find(pence) == Sa1.end()) {
+                retAnswer += calcAnswer(pence, 0);
+                Sa1.insert(N - pence);
             }
-            i += 1;
+            if ((pence == N / 2) || (Sa1.find(N - pence) == Sa1.end())) {
+                retAnswer += calcAnswer(N - pence);
+                Sa1.insert(N - pence);
+            }
         }
         return retAnswer;
-    } 
-    //
-    long long int calcAnswerAuxB2(int k, int N, 
-    set <int> Sb1) {
-        long long int retAnswer = 0;
-        //
-        if (N > k) {
-            retAnswer += calcAnswerAuxC1(k, N, Sb1);
-        }
-        //
-        int m = (N % k);
-        int n1 = Sa1.size();
-        //
-        set <int>::iterator itr;
-        //
-        itr = Sa1.find(m);
-        //
-        // ```N % k``` calc since not in Sa1
-        //
-        if (itr == Sa1.end()) {
-            retAnswer += calcAnswer(m, 0);
-            if (MAX_LIMIT > n1) {
-                Sa1.insert(m);
-            }
-        }
-        //
-        return (retAnswer);
-    }
-    //
-    //
-    int calcAnswerAuxA2(int k, int N, 
-    set <int> Sb1){
-        //
-        long long int retAnswer = 0;
-        //
-        retAnswer += calcAnswerAuxB2(k, N, Sb1);
-        //
-        return (retAnswer);
-    }
-    //
-    long long int calcAnswerAuxA1(long long int N) {
-        //
-        long long int retAnswer = 0;
-        //
-        int k;
-        //
-        //
-        // if (calcAnswerAuxB1(N)) {
-        //     retAnswer += 1;
-        // }
-        //        
-        //
-        set <int>::iterator itr1;
-        //
-        set <int> Sb1;
-        //
-        // iterating over all coins changes
-        for (itr1 = S1.begin(); itr1 != S1.end(); itr1++) {
-            k = (*itr1);
-            Sb1.insert(k);
-            retAnswer += calcAnswerAuxA2(k, N, Sb1);
-        }
-        //
-        return (retAnswer);
     }
     //
     //
@@ -123,11 +54,9 @@ public:
         long long int retAnswer = 0;
         //
         if (N == 1) {
-            Sa1.insert(N);
             return 1;
         }
-        if (N == 2) {
-            Sa1.insert(N);
+        else if (N == 2) {
             return 2;
         }
         //
