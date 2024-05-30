334d510a8e9bf331fe00ff8058786506ad822842
= <=>
2024-05-30 12:01:37 +0530

    Update euler031_2.cpp
    fix(3.1):
    (1) renaming,
    (2) sets

--- hackerrank/euler/euler031/euler031_2.cpp
+++ hackerrank/euler/euler031/euler031_2.cpp
@@ -1,45 +1,66 @@
 #include <iostream>
 #include <set>
 #include <math.h>
-#include <unordered_map>
+#include <set>
 
 using namespace std;
 
 class SolutionM1 {
 private:
     set <long long int> S1 = {1, 2, 5, 10, 20, 50, 100, 200};
-    unordered_map < pair<long long int, long long int> > Sa1 = {};
+    set < long long int > Sv1 = {};
+    set < long long int > Sk1 = {};
     long long int LIMIT = 100000007;
     long long int MAX_LIMIT;
 
 public:
     SolutionM1() {
-        MAX_LIMIT = Sa1.max_size();
+        MAX_LIMIT = Sv1.max_size();
     }
     
     ~SolutionM1() {
-        Sa1.clear();
+        Sv1.clear();
     }
     //
     // N in S1
     bool calcAnswerAuxB1(long long int N) {
         return (S1.find(N) == S1.end());
     }
+
+    long long int keyToVal(set <long long int>::iterator itrk1) {
+        long long int retAnswer = 0;
+        long long int m1 = (*itrk1); 
+        int i = 0;
+        set < long long int >::iterator itrk4;
+        itrk4 = Sk1.begin();
+        set <long long int >::iterator itrv1;
+        //
+        for (itrv1 = Sv1.begin; itrv1 != itrv1.end(); itrv1++) {
+            // pass
+            if ((*itrk4) == m1) {
+                retAnswer += (*itrv1);
+                break;
+            }
+            itrk4++;
+        }
+        return retAnswer;
+    }
     
     long long int calcAnswer(long long int N) {
         //
         long long int retAnswer = 0;
         //
-        Sa1.insert(make_pair(1, 1));
         //
         if (N == 1) {
-            return Sa1[1];
+            Sk1.insert(1);
+            return 1;
         }
         //
-        unordered_map < pair <long long int, long long int> >::iterator itrB1;
-        itrB1 = Sa1.find(N);
+        set < long long int >::iterator itrk1;
+        itrk1 = Sk1.find(N);
         //
-        if (itrB1 == Sa1.end()) {
+        //
+        if (itrk1 == Sk1.end()) {
             if (calcAnswerAuxB1(N)) {
                 retAnswer += 1;
             }
@@ -57,42 +78,50 @@ public:
                 i = 1;
                 numA1 = N;
                 //
-                unordered_map < pair<long long int, long long int> >::iterator itrA1;
+                set < long long int >::iterator itrk2;
                 //
                 while (numA1 > 0) {
                     numA1 = N - (i * num3);
                     if (numA1 > 0) {
-                        itrA1 = Sa1.find(numA1);
-                        if (itrA1 == Sa1.end()) {
+                        itrk2 = Sk1.find(numA1);
+                        if (itrk2 == Sv1.end()) {
+                            //
+                            Sk1.insert(numA1);
+                            //
                             num1 = calcAnswer(numA1);
                             //
                             retAnswer += num1;
                             //
-                            Sa1.insert(make_pair(numA1, num1));
+                            Sv1.insert(num1);
                             //
                         }
                         else {
-                            retAnswer += (*itrA1).second;
+
                         }   
                     }
                     i += 1;
                 }
                 // calc N mod k
-                unordered_map < pair <long long int, long long int> > itrC1;
+                set < long long int >::iterator itrk3;
                 //
                 if (num4 != 0) {
-                    itrC1 = Sa1.find(num4);
-                    if (itrC1 == Sa1.end()) {
+                    itrk3 = Sk1.find(num4);
+                    if (itrk3 == Sk1.end()) {
+                        Sk1.insert(num4);
                         num2 = calcAnswer(num4);
                         retAnswer += num2;
-                        Sa1.insert(make_pair(num4, num2));
+                        Sv1.insert(num2);
                     }
                     else {
-                        retAnswer += (*itrC1).second;
+                        
                     }
                 }
             }
-            Sa1.insert(make_pair(N, retAnswer));
+            Sv1.insert(make_pair(N, retAnswer));
+        }
+        else {
+            retAnswer += keyToVal(itrk1);
+            //
         }
         return retAnswer;
 };
