6c2617befffd51e2361a678947fd518a270716a6
= <=>
2024-05-30 19:10:38 +0530

    euler024.cpp
    tested

--- hackerrank/euler/euler024/euler024_2.cpp
+++ hackerrank/euler/euler024/euler024_2.cpp
@@ -7,10 +7,12 @@ using namespace std;
 
 class SolutionHelper {
 public:
-    vector <char> charV1 = {'a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm'}; 
+    vector <char> charV1 = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'}; 
     vector <long long int> numV1 = {};
     vector <string> strV1 = {};
     long long int numA1 = 13;
+    string S = "abcdefghijklm";
+    string Y = "mlkjihgfedcba";
 
     long long int factorial(long long int n) {
         if (n == 0) {
@@ -23,14 +25,10 @@ public:
     }
 
     void calcNumS(long long int m) {
-        for (long long int i = 1; i <= m; i += 1) {
+        for (long long int i = 0; i <= m; i += 1) {
             numV1.push_back(factorial(i));
         }
         return;
-    }
-
-    long long int calcOffsetAux(long long int m) {
-        return numV1[m];
     }
 
     string calcStrSAux(long long int m) { // m no of digits
@@ -59,7 +57,16 @@ public:
 
 class SolutionA1: public SolutionHelper {
 public:
-    long long int calcOffset(long long int N, long long int m) {
+    SolutionA1() {
+        calcNumS(numA1);
+        calcStrS(numA1);
+    }
+    
+    long long int calcOffsetAux(long long int m) {
+        return numV1[m - 1];
+    }
+
+    long long int calcOffset(long long int N, int m) {
         long long int offset;
         offset = N - calcOffsetAux(m);
         return offset;
@@ -74,10 +81,15 @@ public:
         return numA1;
     }
 
-    string calcAnswer(long long int N) {
-        long long int offset = calcOffset(N, numA1);
-        long long int m = calcAnswerAux(offset);
-        return strV1[m];
+    string calcAnswer(long long int N, long long int m) {
+        if (N == 1) {
+            return S;
+        }
+        string str1;
+        long long int offset = calcOffset(N, m);
+        long long int n1 = calcAnswerAux(offset);
+        //
+        return to_string(offset);
     }
 };
 
@@ -88,6 +100,6 @@ int main() {
         long long int N;
         cin >> N;
         SolutionA1 SolA1;
-        cout << SolA1.calcAnswer(N) << endl;
+        cout << SolA1.calcAnswer(N, 13) << endl;
     }
 }
