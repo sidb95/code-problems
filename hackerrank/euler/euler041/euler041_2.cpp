9000309936520915218944c76eced7dee7b79359
Siddharth Bhatore <=>
2024-06-07 23:09:29 +0530

    modify euler041_2.cpp

--- hackerrank/euler/euler041/euler041_2.cpp
+++ hackerrank/euler/euler041/euler041_2.cpp
@@ -1,18 +1,92 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
+#include <set>
 
 using namespace std;
 
+long long int MAX_LENGTH = 100000;
+//
+set <long long int> vis;
+//
 bool isPandigitalPrime(long long int n);
+bool isPrime(long long int n);
 
-long long int calcAnswer(long long int N) {
-    for (int i = N; i >= 10; i += 1) {
-        if (isPandigitalPrime(i)) {
-            return i;
+bool isPrime(long long int n) {
+    bool retPropn = true;
+    if (vis.find(n) != vis.end()) {
+        retPropn = false;
+    }
+    else {
+        if (n == 1) {
+            retPropn = false;
+        }
+        else if (n == 2) {
+            retPropn = true;
+        }
+        else if (n % 2 == 0) {
+            retPropn = false;
+        }
+        else {
+            long long int powerN1 = pow(n, 0.5);
+            for (long long int i = 3; i <= powerN1; i += 2) {
+                int l = vis.size();
+                if (n % i == 0) {
+                    retPropn = false;
+                    break;
+                }
+                else {
+                    if (MAX_LENGTH > l) {
+                        for (long long int j = 2 * i; j < powerN1; j += i) {
+                            vis.insert(j);
+                        }
+                    }
+                }
+            }
         }
     }
-    return -1;
+    return retPropn;
 }
+class SolutionS1 {
+public:
+    vector<string> V;
+
+    SolutionS1() {
+        string str = "1";
+        for(int i = 2; i <= 9; i += 1) {
+            str += to_string(i);
+            V.push_back(str);
+        }
+    }
+
+    bool isPandigitalPrime(long long int n) {
+        string str = to_string(n);
+        int l = V.size();
+        bool retPropn = false;
+        sort(str.begin(), str.end());
+        for (int i = 0; i < l; i += 1) {
+            if (str == V[i]) {
+                if (isPrime(n)) {
+                    retPropn = true;
+                    break;
+                }
+            }
+        }
+        return retPropn;
+    }
+
+    long long int calcAnswer(long long int N) {
+        long long int answer;
+        for (long long int i = N; i >= 10; i -= 1) {
+            if (isPandigitalPrime(i)) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+};
 
 int main() {
     int t, T;
@@ -20,7 +94,8 @@ int main() {
     for (int t = 0; t < T; t += 1) {
         long long int N;
         cin >> N;
-        cout << calcAnswer(N) << endl;
+        SolutionS1 Sol1;
+        cout << Sol1.calcAnswer(N) << endl;
     }
     return 0;
 }
