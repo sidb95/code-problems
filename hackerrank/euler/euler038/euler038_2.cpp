6cc60a9d5015b1d5e746b00987fb0b5a6823ef44
Siddharth Bhatore <=>
2024-06-07 14:54:10 +0530

    euler038

---
+++ hackerrank/euler/euler038/euler038_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+long long int calcAnswer(long long int N, long long int K) {
+    
+}
+
+int main() {
+    long long int N, K;
+    cin >> N >> K;
+    cout << calcAnswer(N, K) << endl;
+    return 0;
+}
