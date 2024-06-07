f8e4b169c05b1a082b68f65648000623180a22b3
Siddharth Bhatore <=>
2024-06-07 16:44:31 +0530

    euler039: ITER2 template

---
+++ hackerrank/euler/euler039/euler039_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+long long int calcAnswer(long long int N) {
+    
+}
+
+int main() {
+    int t, T;
+    cin >> T;
+    for (t = 0; t < T; t += 1) {
+        long long int N;
+        cin >> N;
+        cout << calcAnswer(N) << endl;
+    }
+    return 0;
+}
