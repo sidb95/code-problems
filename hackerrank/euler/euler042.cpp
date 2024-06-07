ab0bf791a35db670d90a7dc518b81d096d769c97
Siddharth Bhatore <=>
2024-06-08 00:57:41 +0530

    modify euler042, create euler043, euler043_2.cpp

--- hackerrank/euler/euler042.cpp
+++ hackerrank/euler/euler042.cpp
@@ -2,5 +2,39 @@
 euler042
 bhatoresiddharth@gmail.com
 sidb95
-07 June 2024
+07, 08 June 2024
 */
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+long long int calcAnswer(long long int n) {
+    long long int answer;
+    double a = pow(1 + (4 * n), 0.5);
+    long long int b = pow(1 + (4 * n), 0.5);
+    if (a - b == 0) {
+        long long int c = (a - 1) / 2;
+        if (c >= 1) {
+            answer = c;
+        }
+        else {
+            answer = -1;
+        }
+    }
+    else {
+        answer = -1;
+    }
+    return answer;
+}
+
+int main() {
+    int t, T;
+    cin >> T;
+    for (t = 0; t < T; t += 1) {
+        long long int tnum;
+        cin >> tnum;
+        cout << calcAnswer(tnum) << endl;
+    }
+}
---
+++ hackerrank/euler/euler043.cpp
@@ -0,0 +1,46 @@
+/*
+euler043
+bhatoresiddharth@gmail.com
+sidb95
+08 June 2024
+*/
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Divisibility {
+public:
+    bool divByTwo(long long int n) {
+        long long int y = 1;
+        return (n & y == 0);
+    }
+
+    bool divByThree(long long int n) {
+        long long int answer = 0;
+        while (n != 0) {
+            answer += (n % 10);
+            n /= 10;
+        }
+        return ((answer % 3) == 0);
+    }
+
+    bool divByFour(long long int n) {
+        long long int answer = 0;
+        for (int i = 0; (i < 2) && (n != 0); i += 1) {
+            answer += (pow(10, i)) * (n % 10);
+            n /= 10;
+        }
+        return ((answer % 4) == 0);
+    }
+
+    bool divByFive(long long int n) {
+        return ((n % 10) == 0) || ((n % 10) == 5);
+    }
+};
+
+int main() {
+    return 0;
+}
+
