2af429428c0ff84535f676d377e28be769d013d6
Siddharth Bhatore <=>
2024-06-08 12:02:35 +0530

    update euler044

--- hackerrank/euler/euler044.cpp
+++ hackerrank/euler/euler044.cpp
@@ -1,10 +1,3 @@
-/*
-euler044
-bhatoresiddharth@gmail.com
-sidb95
-08 June 2024
-*/
-
 #include <iostream>
 #include <vector>
 #include <math.h>
@@ -12,10 +5,10 @@ sidb95
 
 using namespace std;
 
-vector <int> calcAnswer(int N, int K);
-bool isPentagonal(int N);
+vector <long long int> calcAnswer(long long int N, long long int K);
+bool isPentagonal(long long int N);
 
-bool isPentagonal(int N) {
+bool isPentagonal(long long int N) {
     bool retPropn = false;
     double a = pow(1 + (24 * N), 0.5);
     long long int b = pow(1 + (24 * N), 0.5);
@@ -34,16 +27,16 @@ bool isPentagonal(int N) {
 }
 
 
-vector <int> calcAnswer(int N, int K) {
-    vector <int> V1;
-    int M;
+vector <long long int> calcAnswer(long long int N, long long int K) {
+    vector <long long int> V1;
+    long long int M;
     //
     N -= 1;
     //
     while (N >= (K + 1)) {
-        int pentgl1 = (((N) * ((3 * (N)) - 1))) / 2;
+        long long int pentgl1 = (((N) * ((3 * (N)) - 1))) / 2;
         M = N - K;
-        int pentgl2 = (((M) * ((3 * (M)) - 1))) / 2;
+        long long int pentgl2 = (((M) * ((3 * (M)) - 1))) / 2;
         if (isPentagonal((pentgl1 - pentgl2))) {
             V1.push_back(N);
         }
@@ -56,14 +49,16 @@ vector <int> calcAnswer(int N, int K) {
 }
 
 int main() {
-    int N, K;
+    long long int N, K;
     cin >> N >> K;
-    vector <int> V1;
+    vector <long long int> V1;
     V1 = calcAnswer(N, K);
     sort(V1.begin(), V1.end());
-    int n = V1.size();
-    for (int i = 0; i < n; i += 1) {
-        cout << (((3 * V1[i]) - 1) * V1[i]) / 2 << endl;
+    long long int n = V1.size();
+    long long int retValue;
+    for (long long int i = 0; i < n; i += 1) {
+        retValue = (((3 * V1[i]) - 1) * V1[i]) / 2;
+        cout << retValue << endl;
     }
     return 0;
 }
