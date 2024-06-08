22bc37b99b717d23f682b771fb9877e4f3ab955b
Siddharth Bhatore <=>
2024-06-08 10:09:18 +0530

    modify euler043.cpp, euler043_2.cpp: ITER2

--- hackerrank/euler/euler043/euler043_2.cpp
+++ hackerrank/euler/euler043/euler043_2.cpp
@@ -108,7 +108,7 @@ public:
 
     long long int calcAnswer(long long int N) {
         long long int sum = 0;
-        for (int i = 0; i <= N; i += 1) {
+        for (long long int i = 100; i <= pow(10, N + 1) - 1; i += 1) {
             if (calcAnswerAuxA1(i)) {
                 sum += i;
             }
