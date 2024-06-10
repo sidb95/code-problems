ada5722c07169d318fd918a8efa592bdc38a856b
Siddharth Bhatore <=>
2024-06-10 11:46:21 +0530

    modify euler047

--- hackerrank/euler/euler047.cpp
+++ hackerrank/euler/euler047.cpp
@@ -7,10 +7,12 @@ sidb95
 
 #include <iostream>
 #include <set>
+#include <math.h>
 
 using namespace std;
 
 set <int> calcAnswer(int N, int K);
+int calcAnswerAux(int i, int j, int l, set <int>& primes);
 set <int> findPrimes(set <int>& primes, int n);
 bool isPrime(int n);
 
@@ -39,7 +41,7 @@ bool isPrime(int n) {
 
 set <int> findPrimes(set <int>& primes, int n) {
     primes.insert(2);
-    for (int i = 3; i <= pow(n, 0.5); i += 2) {
+    for (int i = 3; i <= n; i += 2) {
         if (isPrime(i)) {
             primes.insert(i);
         }
@@ -47,30 +49,38 @@ set <int> findPrimes(set <int>& primes, int n) {
     return primes;
 }
 
+int calcAnswerAux(int q, int j, int sum, set <int>& primes) {
+    //
+    for (int l = 2; l < q; l += 1) {
+        if ((q % l) == 0) {
+            if (primes.find(l) != primes.end()) {
+                sum += 1;
+            }
+        }
+    }
+    //
+    return sum;
+}
+
 /*
 calculates the set of all the required numbers
 */
 // function ```calcAnswer```
-set <int> calcAnswer(int N, int K, set <int>& primes) {
+set <int> calcAnswer(int N, int K) {
     set <int> retAnswer = {};
     int m, sum, sumTwo;
+    set <int> primes;
+    //
+    primes = findPrimes(primes, N + K - 1);
     //
     for (int j = 2; j <= N; j += 1) {
         sumTwo = 0;
         m = j + K - 1;
         //
-        for (int k = j; k <= m; k += 1) {
+        for (int q = j; q <= m; q += 1) {
             //
-            if (k >= 1) {
-                sum += 1;
-            }
-            //
-            for (int l = 3; l <= (k / 2), l += 2) {
-                if (primes.find(l)) {
-                    sum += 1;
-                }
-            }
-            //
+            sum = 0;
+            sum = calcAnswerAux(q, j, sum, primes);
             if (sum == K) {
                 sumTwo += 1;
             }
@@ -83,12 +93,11 @@ set <int> calcAnswer(int N, int K, set <int>& primes) {
 }
 
 int main() {
-    long long int N, K;
+    int N, K;
     cin >> N >> K;
     set <int> primes;
     set <int> answer;
-    primes = findPrimes(primes, N + K);
-    answer = calcAnswer(N, K, primes);
+    answer = calcAnswer(N, K);
     set <int>::iterator itr;
     for (itr = answer.begin(); itr != answer.end(); itr++) {
         cout << (*itr) << endl;
