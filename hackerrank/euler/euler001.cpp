6e72eb6cfcccf9580ba10d278a7041dd59653ff3
= <=>
2024-06-12 08:31:00 +0530

    modify euler001

--- hackerrank/euler/euler001.cpp
+++ hackerrank/euler/euler001.cpp
@@ -5,38 +5,19 @@ bhatoresiddharth@gmail.com
 03, 12 June 2024
 */
 
-/*
-euler001
-bhatoresiddharth@gmail.com
-20 May 2024
-03, 12 June 2024
-*/
-
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-
-bool divBy5(int n) {
-    return ((n % 10) == 0) || ((n % 10) == 5);
-}
-
-
-long long int sumMultiples(int n) {
+long long int sumMultiples(long long int n) {
     long long int answerSum = 0;
-    for (int i = 3; i < n; i += 3) {
-        answerSum += i;
-        if (divBy5(i + 1)) {
-            if ((i + 1) < n) {
-                answerSum += (i + 1);
-            }
-        }
-        else if (divBy5(i + 2)) {
-            if ((i + 2) < n) {
-                answerSum += (i + 2);
-            }
-        }
-    }
+    n = n - 1;
+    //
+    answerSum = (((n / 3) * (n / 3 + 1)) / 2) * 3 + 
+    (((n / 5) * (n / 5 + 1)) / 2) * 5 -
+    (((n / 15) * (n / 15 + 1))/ 2 ) * 15;
+    //
     return answerSum;
 }
 
@@ -44,7 +25,7 @@ int main(){
     int t;
     cin >> t;
     for(int a0 = 0; a0 < t; a0++){
-        int n;
+        long long int n;
         cin >> n;
         cout << sumMultiples(n) << endl;
     }
