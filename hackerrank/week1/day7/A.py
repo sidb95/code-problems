153b9fc0a0425615e5d53243cce04ab24d1442ee
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-24 19:53:12 +0530

    commit

---
+++ hackerrank/week1/day7/A.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def preOrderAux(root, arr):
+    if (root is None):
+        return arr
+    else:
+        arr.append(root.info)
+        arr = preOrderAux(root.left, arr)
+        arr = preOrderAux(root.right, arr)
+        return arr
+
+def preOrder(root):
+    vals = preOrderAux(root, [])
+    s = ""
+    for val in vals:
+        s += str(val) + ' '
+    print(s)
+
+    
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
