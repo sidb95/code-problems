a29d3c3dce0db948a498ba9be252618087b9cba4
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-25 14:50:16 +0530

    Update A.py

--- codeforces/961/A.py
+++ codeforces/961/A.py
@@ -2,6 +2,12 @@ class Solution:
   pass
 
 def __main__():
-  pass
+  T = int(input())
+  for t in range(0, T):
+    n, k = map(int, input().split())
+    if (k <= ((n * n) - ((2 * n) - 1))):
+      print(0)
+    else:
+      print(k - ((n * n) - ((2 * n) - 1)))
 
 __main__()
