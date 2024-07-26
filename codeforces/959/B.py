9c8b3206efc59fabd954a93ddeba0dea9b52e64d
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-27 04:29:56 +0530

    minor changes

---
+++ codeforces/959/B.py
@@ -0,0 +1,28 @@
+class Print:
+  def __init__(self):
+    pass
+  
+  def printString(self, s: str) -> None:
+    print(s)
+    return None
+  
+  def printCharacter(self, c: chr) -> None:
+    print(c, end = "")
+    return None
+
+  def lengthString(self, s: str) -> None:
+    return len(s)
+  
+
+class Solution:
+  def __init__(self):
+    pass
+  
+  def mainB1(self):
+    pass
+  
+def __main__():
+  T = input()
+  T = int(T)
+  for t in range(0, T):
+    pass
--- codeforces/961/A.py
+++ codeforces/961/A.py
@@ -3,11 +3,14 @@ class Solution:
 
 def __main__():
   T = int(input())
+  #
   for t in range(0, T):
     n, k = map(int, input().split())
+    #
     if (k <= ((n * n) - ((2 * n) - 1))):
       print(0)
     else:
       print(k - ((n * n) - ((2 * n) - 1)))
+  return
 
 __main__()
