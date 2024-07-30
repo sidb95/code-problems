e25ab1ac8dca35df3b4ca919f27a32de56dfe18f
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-30 17:02:57 +0530

    Update euler009.cpp

--- hackerrank/euler/euler009.cpp
+++ hackerrank/euler/euler009.cpp
@@ -8,17 +8,17 @@ sidb95
 
 #include <iostream>
 #include <math.h>
-#include <set>
+#include <unordered_map>
 
 using namespace std;
 
 int calcAnswer(int x) {
     int n1 = x;
-    set <int> VIS;
+    unordered_map <int, bool> VIS;
     for (int i = 1; i <= n1; i++) {
-        VIS.insert(i);
+        VIS[i] = true;
     }
-    set <int>::iterator itrVIS;
+    unordered_map <int, bool>::iterator itrVIS;
     int retAnswer = -1;
     int productVar;
     bool prop1, prop2, prop3;
@@ -26,7 +26,7 @@ int calcAnswer(int x) {
         for (int j = i + 1; j <= (n1 - 1); j++) {
             itrVIS = VIS.find(n1 - (i + j));
             if (itrVIS != VIS.end()) {
-                int k = (*itrVIS);
+                int k = (itrVIS->first);
                 prop1 = (k > j);
                 if (prop1) {
                     prop2 = ((i * i) + (j * j) == (k * k));
