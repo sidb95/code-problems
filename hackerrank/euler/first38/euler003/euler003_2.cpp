590c1041acaabfbcfd5ac836293719be408eb209
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-30 15:59:13 +0530

    Update euler003_2.cpp

--- hackerrank/euler/first38/euler003/euler003_2.cpp
+++ hackerrank/euler/first38/euler003/euler003_2.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <math.h>
+#include <unordered_set>
 
 using namespace std;
+
+unordered_set <int> S = {};
+bool SET_LIMIT = false;
 
 long long int gcd(long long int a, long long int b) {
     long long int t, r;
@@ -20,7 +25,7 @@ long long int gcd(long long int a, long long int b) {
     return gcd(a, b);
 }
 
-bool isPrime(int n) {
+bool isPrime(long long int n) {
     bool retPropn = true;
     if (n == 1) {
         retPropn = false;
@@ -32,10 +37,24 @@ bool isPrime(int n) {
         retPropn = false;
     }
     else {
-        for (int i = 3; i <= pow(n, 0.5); i += 2) {
-            if ((n % i) == 0) {
-                retPropn = false;
-                break;
+        long long int num1 = pow(n, 0.5);
+        if (S.size() == 1000000000000) {
+            SET_LIMIT = true;
+        }
+        for (long long int i = 3; i <= num1; i += 2) {
+            if (S.find(i) == S.end()) {
+                if ((n % i) == 0) {
+                    retPropn = false;
+                    break;
+                }
+            }
+            else {
+                long long int j = 2 * i;
+                
+                while ((!SET_LIMIT) && j <= num1) {
+                    S.insert(j);
+                    j += i;
+                }
             }
         }
     }
@@ -48,11 +67,10 @@ int main() {
     for (t = 0; t < T; t += 1) {
         long long int N;
         cin >> N;
-        long long int answer, num1;
-        for (long long int i = N; i >= 1; i -= 1) {
-            num1 = gcd(N, i);
-            if (num1 == i) {
-                if (isPrime(num1)) {
+        long long int answer;
+        for (long long int i = (N % 2 == 0) ? (N - 1):N; i >= 1; i -= 2) {
+            if (N % i == 0) {
+                if (isPrime(i)) {
                     answer = i;
                     break;
                 }
