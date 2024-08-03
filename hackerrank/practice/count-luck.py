315196e5bc9ed0337c8e20874633aea6ca8da1cf
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-03 13:03:08 +0530

    Update count-luck.py

--- hackerrank/practice/count-luck.py
+++ hackerrank/practice/count-luck.py
@@ -1,55 +1,76 @@
-def dfs(matrix, m, n, i, j, path):
+def inBounds(m, n, i, j):
+    return ((i >= 0 and i < m) and (j >= 0 and j < n))
+
+def dfs(matrix, m, n, i, j, path, VIS):
+    if ((i, j) in VIS):
+        return (False, path)
     if (matrix[i][j] == 'M' and path != ""):
         return (False, path)
     if (matrix[i][j] == 'X'):
         return (False, path)
     if (matrix[i][j] == '*'):
         return (True, path)
+    VIS[(i, j)] = True
+    count1 = 0
+    A = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+    for a_i in A:
+        if (a_i not in VIS):
+            if (inBounds(m, n, a_i[0], a_i[1])):
+                if (matrix[a_i[0]][a_i[1]] != 'X'):
+                    count1 += 1
     arr = ""
     for l1 in range(0, len(path)):
         arr += path[l1]
-    path += "l"
+    if (count1 > 1):
+        path += "L"
+    else:
+        path += 'l'
     if ((i >= 0 and i < m) and ((j - 1) >= 0 and (j - 1) < n)):
         if (matrix[i][j - 1] != 'X'):
-            retTuple = dfs(matrix, m, n, i, j - 1, path)
-            if (retTuple[0]):
-                return (True, retTuple[1])
+            retTuple1 = dfs(matrix, m, n, i, j - 1, path, VIS)
+            if (retTuple1[0]):
+                return (True, retTuple1[1])
     path = arr
-    path += "r"
+    if (count1 > 1):
+        path += "R"
+    else:
+        path += "r"
     if ((i >= 0 and i < m) and ((j + 1) >= 0 and (j + 1) < n)):
         if (matrix[i][j + 1] != 'X'):
-            retTuple = dfs(matrix, m, n, i, j + 1, path)
-            if (retTuple[0]):
-                return (True, retTuple[1])
+            retTuple2 = dfs(matrix, m, n, i, j + 1, path, VIS)
+            if (retTuple2[0]):
+                return (True, retTuple2[1])
     path = arr
-    path += "u"
+    if (count1 > 1):
+        path += "U"
+    else:
+        path += "u"
     if (((i - 1) >= 0 and (i - 1) < m) and ((j >= 0) and (j < n))):
         if (matrix[i - 1][j] != 'X'):
-            retTuple = dfs(matrix, m, n, i - 1, j, path)
-            if (retTuple[0]):
-                return (True, retTuple[1])
+            retTuple3 = dfs(matrix, m, n, i - 1, j, path, VIS)
+            if (retTuple3[0]):
+                return (True, retTuple3[1])
     path = arr
-    path += "d"
+    if (count1 > 1):
+        path += "D"
+    else:
+        path += "d"
     if (((i + 1) >= 0 and (i + 1) < m) and ((j >= 0) and ((j < n)))):
         if (matrix[i + 1][j] != 'X'):
-            retTuple = dfs(matrix, m, n, i + 1, j, path)
-            if (retTuple[0]):
-                return (True, retTuple[1])
+            retTuple4 = dfs(matrix, m, n, i + 1, j, path, VIS)
+            if (retTuple4[0]):
+                return (True, retTuple4[1])
     return (False, path)
 
 def countLuckAux(path):
-    count = 1
     count1 = 0
-    i = 1
+    i = 0
     n = len(path)
     while (i < n):
-        if (path[i] != path[i - 1]):
+        if (path[i] >= 'A' and path[i] <= 'Z'):
             count1 += 1
         i += 1
-    if (count1 == 0):
-        return count
-    else:
-        return count1
+    return count1
 
 def countLuck(matrix, k):
     m = len(matrix)
@@ -58,7 +79,7 @@ def countLuck(matrix, k):
     for i in range(0, m):
         for j in range(0, n):
             if (matrix[i][j] == 'M'):
-                d1 = dfs(matrix, m, n, i, j, "")
+                d1 = dfs(matrix, m, n, i, j, "", {})
                 path = d1[1]
                 print(path)
                 if (countLuckAux(path) == k):
