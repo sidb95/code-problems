3788dfd44713c6a427f619c7bb613751f283fab5
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-04 17:18:53 +0530

    Update library-fine.py

--- hackerrank/practice/library-fine.py
+++ hackerrank/practice/library-fine.py
@@ -8,8 +8,8 @@ if d1 < d2: 15 * (d2 - d1)
 def libraryFine(d1, m1, y1, d2, m2, y2):
     if (y1 > y2):
         return 10000
-    if (m1 > m2):
+    if ((y1 == y2) and (m1 > m2)):
         return (500 * (m1 - m2))
-    if (d1 > d2):
+    if ((y1 == y2) and (m1 == m2) and (d1 > d2)):
         return (15 * (d1 - d2))
     return 0
