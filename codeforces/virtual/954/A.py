ae8cb85f0c630263d54016e7f02ad00cecb376b5
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-05 21:13:06 +0530

    954: codeforces

--- codeforces/virtual/954/A.py
+++ codeforces/virtual/954/A.py
@@ -3,22 +3,23 @@ sbhatore
 04 August 2024
 
 # Assumptions:
-#
-#
+# 1. The problem has a solution,
+# 2. The solution is definite, correct and finite,
+# 3. The solution can be coded in Python3
 #
 # In:
-#
+# 1. x1, x2, x3
 #
 #
 #
 # Out:
-#
+# 1.
 #
 #
 # Solution:
-#
-#
-#
+# 1. If the middle point exists, be it
+# 2. if two points coincide, be it
+# 3.
 # pseudocode:
 """
 
@@ -29,7 +30,19 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
-
+        x1, x2, x3 = map(int, input().split())
+        if (x1 == x2):
+            print(abs(x1 - x3))
+        elif (x2 == x3):
+            print(abs(x1 - x3))
+        elif (x1 == x3):
+            print(abs(x1 - x2))
+        else:
+            nums = [x1, x2, x3]
+            nums = sorted(nums)
+            print((abs(nums[0] - nums[1])) + abs(nums[2] - nums[1]))
+        #
+    #
+    return
 
 __main__()
--- codeforces/virtual/954/B.py
+++ codeforces/virtual/954/B.py
@@ -7,12 +7,12 @@ sbhatore
 #
 #
 # In:
-#
-#
+# 1. n, m
+# 2. nums
 #
 #
 # Out:
-#
+# 1. after stablization
 #
 #
 # Solution:
@@ -21,15 +21,71 @@ sbhatore
 #
 # pseudocode:
 """
+import copy
 
-def calcAnswer():
-    pass
+def inBounds(i, j, n, m):
+    return ((i >= 0) and (i < n) and (j >= 0) and (j < m))
 
+def calcAnswer(A, arr, i, j, n, m):
+    if (not inBounds(i, j, n, m)):
+        return A
+    FLAG = True
+    maxVal = 0
+    for k in range(0, len(arr)):
+        if (inBounds(arr[k][0], arr[k][1], n, m)):
+            if ((A[arr[k][0]][arr[k][1]] >= A[i][j])):
+                FLAG = False
+                break
+            else:
+                maxVal = max(maxVal, A[arr[k][0]][arr[k][1]])
+            #
+        #
+    #
+    if (FLAG):
+        A[i][j] = maxVal
+    return A
 
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n, m = map(int, input().split())
+        nums = []
+        D = []
+        #
+        for i in range(0, n):
+            TEMP = input()
+            D.append(TEMP)
+        for i in range(0, len(D)):
+            E = list(map(int, D[i].split()))
+            nums.append(E)
+        #
+        FLAG = True
+        # while (FLAG):
+        i = 0
+        j = 0
+        #
+        while ((i < n) or (j < m)):
+            arr = ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1))
+            A = calcAnswer(nums, arr, i, j, n, m)
+            for k in range(j + 1, m):
+                arr = ((i + 1, k), (i - 1, k), (i, k + 1), (i, k - 1))
+                A = calcAnswer(A, arr, i, k, n, m)
+            for k in range(i + 1, n):
+                arr = ((k + 1, j), (k - 1, j), (k, j + 1), (k, j - 1))
+                A = calcAnswer(A, arr, k, j, n, m)
+            #
+            i += 1
+            j += 1
+        nums = A
+            #
+        #
+        for i in range(0, n):
+            for j in range(0, m - 1):
+                print(nums[i][j], end=" ")
+            print(nums[i][m - 1])
+        #   
+    #
+    return
 
 
 __main__()
