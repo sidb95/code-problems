4358f30e634cc8b7655dadc588850889191856c0
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-06 08:27:47 +0530

    carrom board using racket: core/racket-core/learn

--- core/core-racket/learn/carrom_board.rkt
+++ core/core-racket/learn/carrom_board.rkt
@@ -1,7 +1,56 @@
 #lang slideshow
 
-; circle is a procedure
+(require racket/draw)
 
-(define circ (circle 10))
-(define sq (colorize (filled-rectangle 25 25) "orange"))
-(vc-append (hc-append 10 circ circ) (vc-append sq (hc-append 10 circ circ)))
+(define target (make-bitmap 30 30)) ; A 30x30 bitmap
+(define dc (new bitmap-dc% [bitmap target]))
+
+; board
+(send dc set-brush "gold" 'solid)
+(send dc set-pen "black" 1 'solid)
+(send dc draw-rectangle
+      0 0   ; Top-left at (0, 10), 10 pixels down from top-left
+      30 30) ; 30 pixels wide and 10 pixels high
+(send dc draw-line
+      7 5
+      23 5)
+(send dc draw-line
+      7 5
+      7 25)
+(send dc draw-line
+      7 25
+      23 25)
+(send dc draw-line
+      23 5
+      23 25)
+; circles
+(send dc set-brush "brown" 'solid)
+(send dc set-pen "black" 1 'solid)
+(send dc draw-ellipse 0 0 7 7)
+(send dc draw-ellipse 0 23 7 7)
+(send dc draw-ellipse 23 0 7 7)
+(send dc draw-ellipse 23 23 7 7)
+(send dc set-brush "red" 'solid)
+(send dc draw-ellipse 7 5 5 5)
+(send dc draw-ellipse 20 5 5 5)
+(send dc draw-ellipse 7 20 5 5)
+(send dc draw-ellipse 20 20 5 5)
+; lines
+(send dc set-pen "black" 0.01 'solid)
+(send dc draw-line
+      10 15
+      20 15)
+(send dc draw-line
+      15 10
+      15 20)
+(send dc draw-line
+      12 12
+      18 18)
+(send dc draw-line
+      12 18
+      18 12)
+; center
+(send dc draw-ellipse 13 13 5 5)
+
+; save in file
+(send target save-file "box.png" 'png)
