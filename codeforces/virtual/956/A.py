bced5cf7bafaea6ce87a865e96b1765dd89289fe
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-08 15:47:58 +0530

    virtual 956: codeforces initial commit

---
+++ codeforces/virtual/956/A.py
@@ -0,0 +1,35 @@
+"""
+sbhatore
+08 August 2024
+
+# Assumptions:
+#
+#
+#
+# In:
+#
+#
+#
+#
+# Out:
+#
+#
+#
+# Solution:
+#
+#
+#
+# pseudocode:
+"""
+
+def calcAnswer():
+    pass
+
+
+def __main__():
+    T = int(input())
+    for t in range(0, T):
+        pass
+
+
+__main__()
