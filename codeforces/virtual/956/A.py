a1bc74249f10fa7ffe0d87c45409ab688a9232b4
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-09 10:30:03 +0530

    dnastorage.py: codechef/practice/strings

--- codeforces/virtual/956/A.py
+++ codeforces/virtual/956/A.py
@@ -7,16 +7,16 @@ sbhatore
 #
 #
 # In:
-#
+# 1. n
 #
 #
 #
 # Out:
-#
+# 1. a_i, 0 <= i < n (a_i divisible by all (i + 1)s that are factor)
 #
 #
 # Solution:
-#
+# 1. loop from 2 to int(n / 2), multiply all
 #
 #
 # pseudocode:
@@ -29,7 +29,27 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        nums = []
+        nums.append(1)
+        for i in range(2, n + 1):
+            num1 = int(i / 2)
+            num2 = 1
+            for j in range(2, num1 + 1):
+                if (i % j == 0):
+                    num2 *= j
+            num2 *= i
+            nums.append(num2)
+        for i in range(2, n + 1):
+            num3 = 0
+            for j in range(2, n + 1, i):
+                num3 += j
+            nums[i - 1] *= num3
+        for i in range(0, n - 1):
+            print(nums[i], end=" ")
+        print(nums[n - 1])
+    #
+    return
 
 
 __main__()
