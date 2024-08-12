c2293e6e21bc6c19aa4d2441b0e7c13119fee6f6
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-12 21:25:02 +0530

    943: codeforces

---
+++ codeforces/virtual/943/D.py
@@ -0,0 +1,35 @@
+"""
+sbhatore
+10 August 2024
+
+# Assumptions:
+#
+#
+#
+# In:
+#
+#
+#
+#
+# Out:
+#
+#
+#
+# Solution:
+#
+#
+#
+# pseudocode:
+"""
+
+def calcAnswer():
+    pass
+
+
+def __main__():
+    T = int(input())
+    for t in range(0, T):
+        pass
+
+
+__main__()
