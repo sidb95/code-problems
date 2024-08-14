72595f26530e828374b83a15e0e7fff239c24d82
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-14 16:20:56 +0530

    Update A.py

--- codeforces/virtual/966/A.py
+++ codeforces/virtual/966/A.py
@@ -1,22 +1,22 @@
 """
 sbhatore
-10 August 2024
+14 August 2024
 
 # Assumptions:
-#
-#
-#
+# 1. The problem is solvable,
+# 2. The solution is definite, finite and correct,
+# 3. The solution can be coded in Python3.
 # In:
-#
+# 1.
 #
 #
 #
 # Out:
-#
+# 1. 
 #
 #
 # Solution:
-#
+# 1. 
 #
 #
 # pseudocode:
@@ -29,7 +29,22 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        s = str(n)
+        if (len(s) == 1):
+            print("NO")
+        elif (s[0:2] == "10"):
+            if (len(s) == 2):
+                print("NO")
+            elif (s[2] == '0' or (s[2] == '1' and len(s) == 3)):
+                print("NO")
+            else:
+                print("YES")
+        else:
+            print("NO")
+    #
+    return
+
 
 
 __main__()
