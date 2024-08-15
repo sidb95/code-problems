fcfc9514e7331ce1ae2abfaeed30a8c280b7e611
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-15 17:17:52 +0530

    947: virtual codeforces

--- codeforces/virtual/947/A.py
+++ codeforces/virtual/947/A.py
@@ -7,19 +7,25 @@ sbhatore
 #
 #
 # In:
-#
-#
-#
+# 1. T
+# 2. n
+# 3. a_i, 0 <= i < n
 #
 # Out:
+# 1. YES / NO
 #
 #
-#
+rough-work:
+
 # Solution:
-#
+# 1. concatenate the other part in front and check if the array is sorted.
 #
 #
 # pseudocode:
+-
+-
+-
+-
 """
 
 def calcAnswer():
@@ -29,7 +35,27 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        a = list(map(int, input().split()))
+        A = sorted(a)
+        if (a == A):
+            print("YES")
+        else:
+            i = 1
+            FLAG = False
+            while (i < n):
+                if ((a[i:] + a[:i]) == A):
+                    FLAG = True
+                    break        
+                i += 1
+            if (FLAG):
+                print("YES")
+            else:
+                print("NO")
+            #
+        #
+    #
+    return
 
 
 __main__()
--- codeforces/virtual/947/B.py
+++ codeforces/virtual/947/B.py
@@ -1,22 +1,22 @@
 """
 sbhatore
-10 August 2024
+15 August 2024
 
 # Assumptions:
 #
 #
 #
 # In:
-#
-#
-#
+# 1. T
+# 2. n
+# 3. a_i, 0 <= i < n
 #
 # Out:
-#
+# 1. Yes / No
 #
 #
 # Solution:
-#
+# 1. maintain a dict, that stores all the Truth values
 #
 #
 # pseudocode:
@@ -29,7 +29,44 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
-
+        n = int(input())
+        a = list(map(int, input().split()))
+        VIS = []
+        S = {}
+        for i in range(0, n):
+            S[i] = []
+        for i in range(0, n):
+            count = 0
+            for j in range(0, n):
+                if ((a[j] % a[i]) == 0):
+                    S[i].append(True)
+                    count += 1
+                else:
+                    S[i].append(False)
+                #
+            #
+            if (count >= int(n / 2)):
+                VIS.append(i)
+        #
+        FLAG = False
+        for i in VIS:
+            for j in range(0, n):
+                if (j != i):
+                    count = 0
+                    for k in range(0, n):
+                        if (S[i][k] or S[j][k]):
+                            count += 1
+                            if (count == n):
+                                FLAG = True
+                                break
+            if (FLAG):
+                break
+        if (FLAG):
+            print("Yes")
+        else:
+            print("No")
+        #
+    #
+    return
 
 __main__()
