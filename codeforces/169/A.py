bbf741e7195caaba45cc706537c1b86a0efbd7ec
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-17 11:58:37 +0530

    Update A.py

--- codeforces/169/A.py
+++ codeforces/169/A.py
@@ -7,14 +7,19 @@ sbhatore
 # 2. The solution is definite, finite and correct,
 # 3. The solution can be coded in Pyton3.
 # In:
-# 1. 
-#
+# 1. n
+# 2. a_i
 #
 #
 # Out:
-# 1. 
+# 1. YES / NO
 #
 #
+rough-work:
+- 
+- 
+- 
+- 
 # Solution:
 # 1. 
 #
@@ -29,7 +34,21 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        a = list(map(int, input().split()))
+        b = list(set(a))
+        if (len(b) > 2):
+            print("NO")
+        elif (len(b) == 2):
+            if (abs(b[1] - b[0]) >= 2):
+                print("YES")
+            else:
+                print("NO")
+        else:
+            print("NO")
+        #
+    #
+    return
 
 
 __main__()
