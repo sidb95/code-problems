dde55ae0f6431fc0ca5e2d2bdada392acc801bba
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-21 13:08:37 +0530

    virtual 967: codeforces

--- codeforces/virtual/967/A.py
+++ codeforces/virtual/967/A.py
@@ -1,20 +1,23 @@
 """
 sbhatore
-10 August 2024
+21 August 2024
 
 # Assumptions:
-#
-#
-#
+# 1. The problem is solvable,
+# 2. The solution is definite, finite and correct,
+# 3. The solution can be coded in Python3.
 # In:
-#
+# 1. 
 #
 #
 #
 # Out:
+# 1. 
 #
 #
-#
+rough-work:
+- a_i, 0 <= i < n
+- 6 6 6 5 7 7 7
 # Solution:
 #
 #
@@ -29,7 +32,14 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        a = list(map(int, input().split()))
+        maxVal = -1
+        for a_i in a:
+            maxVal = max(maxVal, a.count(a_i))
+        print(n - maxVal)
+    return
+        
 
 
 __main__()
--- codeforces/virtual/967/B.py
+++ codeforces/virtual/967/B.py
@@ -1,6 +1,6 @@
 """
 sbhatore
-10 August 2024
+21 August 2024
 
 # Assumptions:
 #
@@ -15,6 +15,12 @@ sbhatore
 #
 #
 #
+rough-work:
+- a_i
+- 4 1 2 3
+- 2 1
+- 1 2 5 4 3
+- 1 2 3 5 6 4
 # Solution:
 #
 #
@@ -29,7 +35,23 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        if (n == 1):
+            print(1)
+        elif (n % 2 == 0):
+            print(-1)
+        else:
+            a = []
+            for i in range(1, int(n / 2) + 1):
+                a.append(i)
+            for i in range(n, int(n / 2), -1):
+                a.append(i)
+            for j in range(0, n - 1):
+                print(a[j], end=" ")
+            print(a[n - 1])
+        #
+    #
+    return
 
 
 __main__()
