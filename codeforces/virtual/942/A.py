ec652c37cd808c548aa4ae986b2dfd1209e5e298
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-08-28 09:33:06 +0530

    virtual contest 942: codeforces, initial commit

---
+++ codeforces/virtual/942/A.py
@@ -0,0 +1,38 @@
+"""
+sbhatore
+28 August 2024
+
+# Assumptions:
+# 1. The problem is solvable,
+# 2. The solution is definite, finite and correct,
+# 3. The solution can be coded in Python3.
+# In:
+# 1.
+#
+#
+#
+# Out:
+# 1.
+#
+#
+rough-work:
+-
+-
+# Solution:
+#
+#
+#
+# pseudocode:
+"""
+
+def calcAnswer():
+    pass
+
+
+def __main__():
+    T = int(input())
+    for t in range(0, T):
+        pass
+
+
+__main__()
