046408591031d0fc1d3b688fc85454a1ce7aaa57
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-01 09:43:29 +0530

    virtual 923: codeforces, initial commit

--- codeforces/virtual/969/A.py
+++ codeforces/virtual/969/A.py
@@ -31,7 +31,23 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
-
+        l, r = map(int, input().split())
+        num1 = -1
+        if (l % 2 == 0):
+            if (r % 2 == 0):
+                num1 = (r - l - 1)
+            else:
+                num1 = (r - l)
+            #
+        else:
+            if (r % 2 == 0):
+                num1 = (r - l)
+            else:
+                num1 = (r - l + 1)
+            #
+        #
+        print(int((num1 + 1) / 4))
+    #
+    return
 
 __main__()
