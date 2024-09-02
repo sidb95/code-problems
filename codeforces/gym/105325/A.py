53495387eb44715ea7e95fc4640c8c464c57d4a1
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-02 14:24:41 +0530

    spain olympiad: codeforces, initial commit

---
+++ codeforces/gym/105325/A.py
@@ -0,0 +1,37 @@
+"""
+sbhatore
+2 Sep 2024
+
+# Assumptions:
+# 1. The problem is solvable,
+# 2. The solution is definite, finite and correct,
+# 3. The solution can be coded in Python3.
+# In:
+#
+#
+#
+#
+# Out:
+#
+#
+#
+# Solution:
+#
+#
+#
+rough-work:
+-
+# pseudocode:
+"""
+
+def calcAnswer():
+    pass
+
+
+def __main__():
+    T = int(input())
+    for t in range(0, T):
+        pass
+
+
+__main__()
