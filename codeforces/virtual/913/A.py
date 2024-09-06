5651924c27ba3a099ba25a2a59901f3c55479306
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-06 14:36:19 +0530

    virtual 913: codeforces

--- codeforces/virtual/913/A.py
+++ codeforces/virtual/913/A.py
@@ -29,7 +29,21 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        s = input()
+        a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        b = ['1', '2', '3', '4', '5', '6', '7', '8']
+        for elmt in a:
+            if (elmt != s[0]):
+                print(str(elmt) + str(s[1]))
+            #
+        #
+        for elmt in b:
+            if (elmt != s[1]):
+                print(str(s[0]) + str(elmt))
+            #
+        #
+    #
+    return
 
 
 __main__()
--- codeforces/virtual/913/B.py
+++ codeforces/virtual/913/B.py
@@ -1,6 +1,6 @@
 """
 sbhatore
-3 Sep 2024
+6 Sep 2024
 
 # Assumptions:
 #
@@ -16,7 +16,7 @@ sbhatore
 #
 #
 # Solution:
-#
+# 1. place '-' on the place where it is to be deleted
 #
 #
 # pseudocode:
@@ -29,7 +29,43 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        s = input()
+        n = len(s)
+        a = []
+        A = []
+        for i in range(0, n):
+            if (ord(s[i]) >= ord('a') and ord(s[i]) <= ord('z')):
+                if (s[i] == 'b'):
+                    num1 = len(a)
+                    if (num1 != 0):
+                        num2 = a[num1 - 1]
+                        s = s[:num2] + "-" + s[num2 + 1:]
+                        a.pop(num1 - 1)
+                    #
+                else:
+                    a.append(i)
+                #
+            else:
+                if (s[i] == 'B'):
+                    num3 = len(A)
+                    if (num3 != 0):
+                        num4 = A[num3 - 1]
+                        s = s[:num4] + "-" + s[num4 + 1:]
+                        A.pop(num3 - 1)
+                    #
+                else:
+                    A.append(i)
+                #
+            #
+        #
+        for i in range(0, n):
+            if (s[i] != '-' and s[i] != 'b' and s[i] != 'B'):
+                print(s[i], end="")
+            #
+        #
+        print("")
+    #
+    return
 
 
 __main__()
