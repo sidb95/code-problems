f6603719d0afe13e6247fc9a69d8205f440c942d
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-06 17:17:13 +0530

    Update B.py

--- codeforces/virtual/913/B.py
+++ codeforces/virtual/913/B.py
@@ -22,8 +22,21 @@ sbhatore
 # pseudocode:
 """
 
-def calcAnswer():
-    pass
+def calcAnswer(s, a, n, c):
+    itr = 0
+    ptr = 0
+    while (itr < n):
+        while ((itr < n) and (a[itr] != c)):
+            itr += 1
+        ptr = itr
+        while ((ptr >= 0 and ptr < n) and (a[ptr] == c or a[ptr] == '-')):
+            ptr -= 1
+        if (ptr >= 0 and ptr < n):
+            a[ptr] = '-'
+        #
+        itr += 1
+    #
+    return a
 
 
 def __main__():
@@ -33,40 +46,55 @@ def __main__():
         n = len(s)
         a = []
         A = []
+        c = []
+        S = {}
         for i in range(0, n):
-            if (s[i] == 'b'):
-                num1 = -1
-                j = i - 1
-                while ((j >= 0) and ((ord(s[j]) >= ord('A') and ord(s[j]) <= ord('Z')) or (s[j] == '-' or s[j] == 'b' or s[j] == 'B'))):
-                    j -= 1
-                if (j != -1):
-                    num1 = j
-                if (num1 != -1):
-                    if (num1 == 0):
-                        s = "-" + s[1:]
-                    else:
-                        s = s[:num1] + "-" + s[num1 + 1:]
-                #
+            if (s[i] in S):
+                S[s[i]].append(i)
+            else:
+                S[s[i]] = [i]
+            if (ord(s[i]) >= ord('a') and ord(s[i]) <= ord('z')):
+                a.append(s[i])
+            else:
+                A.append(s[i])
             #
-            elif (s[i] == 'B'):
-                num1 = -1
-                j = i - 1
-                while ((j >= 0) and ((ord(s[j]) >= ord('a') and ord(s[j]) <= ord('z')) or (s[j] == '-') or s[j] == 'b' or s[j] == 'B')):
-                    j -= 1
-                if (j != -1):
-                    num1 = j
-                if (num1 != -1):
-                    if (num1 == 0):
-                        s = "-" + s[1:]
-                    else:
-                        s = s[:num1] + "-" + s[num1 + 1:]
+        #
+        a = calcAnswer(s, a, len(a), 'b')
+        A = calcAnswer(s, A, len(A), 'B')
+        itr = 0
+        ptr = 0
+        while ((itr < len(a)) and (ptr < len(A))):
+            while ((itr < len(a)) and (a[itr] == 'b' or a[itr] == '-')):
+                itr += 1
+            #
+            while ((ptr < len(A)) and (A[ptr] == 'B' or A[ptr] == '-')):
+                ptr += 1
+            if (itr != len(a) and ptr != len(A)):
+                if (S[a[itr]][0] < S[A[ptr]][0]):
+                    c.append(a[itr])
+                    S[a[itr]].pop(0)
+                    itr += 1
+                else:
+                    c.append(A[ptr])
+                    S[A[ptr]].pop(0)
+                    ptr += 1
                 #
             #
         #
-        for i in range(0, n):
-            if (s[i] != '-' and s[i] != 'b' and s[i] != 'B'):
-                print(s[i], end="")
+        if (itr == len(a)):
+            while (ptr < len(A)):
+                c.append(A[ptr])
+                ptr += 1
             #
+        else:
+            while (itr < len(a)):
+                c.append(a[itr])
+                itr += 1
+            #
+        #
+        for i in range(0, len(c)):
+            if (c[i] != 'B' and c[i] != 'b' and c[i] != '-'):
+                print(c[i], end="")
         #
         print("")
     #
