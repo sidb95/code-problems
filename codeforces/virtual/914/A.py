6eac527fe8eb83c91ee5e5ecec75a53b6baf3059
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-08 12:47:05 +0530

    Update A.py

--- codeforces/virtual/914/A.py
+++ codeforces/virtual/914/A.py
@@ -29,7 +29,28 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        a, b = map(int, input().split())
+        x_k, y_k = map(int, input().split())
+        x_q, y_q = map(int, input().split())
+        nums1 = (
+                (x_k - a, y_k - b), (x_k + a, y_k + b), (x_k - a, y_k + b), (x_k + a, y_k - b),
+                (x_k - b, y_k - a), (x_k + b, y_k + a), (x_k - b, y_k + a), (x_k + b, y_k - a)
+                )
+        nums2 = (
+                (x_q - a, y_q - b), (x_q + a, y_q + b), (x_q - a, y_q + b), (x_q + a, y_q - b),
+                (x_q - b, y_q - a), (x_q + b, y_q + a), (x_q - b, y_q + a), (x_q + b, y_q - a)
+                )
+        count = 0
+        vis = []
+        for elmt in nums1:
+            if (elmt in nums2 and elmt not in vis):
+                count += 1
+                vis.append(elmt)
+            #
+        #
+        print(count)
+    #
+    return
 
 
 __main__()
