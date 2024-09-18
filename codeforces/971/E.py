35462b310068bb0d410687df7b63cfd930c47860
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-18 16:25:14 +0530

    virtual 972:codeforces, initial commit

--- codeforces/971/E.py
+++ codeforces/971/E.py
@@ -22,14 +22,29 @@ sbhatore
 # pseudocode:
 """
 
-def calcAnswer():
-    pass
+def calcAnswer(mid, n, k):
+    return (2 * mid - n) * k + mid * (mid - 1) + int((n * (n - 1)) / 2)
 
 
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n, k = map(int, input().split())
+        minVal = 1000000001
+        itr = 0
+        ptr = n - 1
+        while (itr <= ptr):
+            mid = itr + int((ptr - itr) / 2)
+            num1 = abs(calcAnswer(mid, n, k))
+            if (abs(num1) < abs(calcAnswer(itr, n, k))):
+                itr = mid + 1
+            else:
+                ptr = mid - 1
+            #
+        #
+        print(min(min(abs(calcAnswer(itr, n, k)), abs(calcAnswer(ptr, n, k))), num1))
+    #
+    return
 
 
 __main__()
