074944d9e61a437c12efc377e4b6597f674fcaf8
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-09-24 10:06:37 +0530

    leetcode practice

--- leetcode/practice/remove-duplicates-from-sorted-array-ii.py
+++ leetcode/practice/remove-duplicates-from-sorted-array-ii.py
@@ -1,48 +1,35 @@
 """
-in: nums
-out: None
-
-2 2 2(itr1) 3(itr2) 3 3 3 4 4
-2 2 3 3 3 3 4 4
+2 (itr1 = 0, count = 0) 2 (itr1 = 1, count = 1) 2 (itr1 = 2, count = 2) 2 3 (itr2) 3 3 4 4
 """
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int: # type: ignore
+    def removeDuplicates(self, nums: List[int]) -> int:
         n = len(nums)
         itr1 = 0
         while ((itr1 < n) and (itr2 < n)):
-            j = itr1
             num1 = nums[itr1]
             count = 0
-            while ((j < n) and (nums[j] == num1) and (count < 2)):
-                j += 1
+            while ((itr1 < n) and (nums[itr1] == num1) and (count < 2)):
+                itr1 += 1
                 count += 1
             #
-            itr2 = j
+            itr2 = itr1
             while ((itr2 < n) and (nums[itr2] == num1)):
                 itr2 += 1
             #
-            k = itr2
-            itr1 = j
-            while (k < n):
-                nums[j] = nums[k]
-                j += 1
-                k += 1
+            if (itr2 == n):
+                break
+            else:
+                itrTemp = itr2
+                while ((itr2 < n) and (itr1 < itrTemp)):
+                    nums[itr1] = nums[itr2]
+                    itr1 += 1
+                    itr2 += 1
+                #
+                itr1 = itrTemp
             #
         #
-        answer = 0
-        i = 0
-        while (i < n):
-            num1 = nums[i]
-            count1 = 0
-            while ((i < n) and (nums[i] == num1)):
-                i += 1
-                count1 += 1
-            #
-            if (count1 >= 2):
-                answer += 2
-            else:
-                answer += 1
-            #
-        # 
-        return answer
+        return
+            
+
+         
