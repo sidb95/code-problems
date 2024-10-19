e0bc6503b112787ebba480c52871a1d6dc8804af
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-10-19 19:16:32 +0530

    codeforces: 978 virtual

---
+++ codeforces/virtual/978/E.py
@@ -0,0 +1,35 @@
+"""
+sbhatore
+3 Sep 2024
+
+# Assumptions:
+#
+#
+#
+# In:
+#
+#
+#
+#
+# Out:
+#
+#
+#
+# Solution:
+#
+#
+#
+# pseudocode:
+"""
+
+def calcAnswer():
+    pass
+
+
+def __main__():
+    T = int(input())
+    for t in range(0, T):
+        pass
+
+
+__main__()
