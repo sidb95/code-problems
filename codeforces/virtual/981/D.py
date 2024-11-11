f31fba8bbc61b28249ed362d9044e7864fb5c0ba
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-11 17:42:29 +0530

    Update D.py

--- codeforces/virtual/981/D.py
+++ codeforces/virtual/981/D.py
@@ -16,20 +16,47 @@ sbhatore
 #
 #
 # Solution:
-#
-#
-#
+# 1. fill up the upper right corner of the dp table
+# 2. Check from left to right, top to bottom if there is a 0 present
+# 3. Take that 0 in the count, then calculate the solution recursively
 # pseudocode:
 """
 
-def calcAnswer():
-    pass
+def calcAnswer(a, dp, i, n):
+    if (i >= n):
+        return 0
+    else:
+        count = 0
+        for p in range(i, n):
+            for q in range(0, len(dp[p])):
+                if (dp[p][q] == 0):
+                    count = max(count, 1 + calcAnswer(a, dp, p + 1 + q, n))
+                #
+            #
+        #
+        return count
 
 
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        a = list(map(int, input().split()))
+        dp = {}
+        for i in range(0, n):
+            dp[i] = [a[i]]
+            sum = a[i]
+            for j in range(i + 1, n):
+                sum += a[j]
+                dp[i] += [sum]
+            #
+        #
+        count = 0
+        for i in range(0, n):
+            count = max(count, calcAnswer(a, dp, i, n))
+        print(count)
+    #
+    return
 
 
 __main__()
