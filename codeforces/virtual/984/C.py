8db6d6dbef9cc0789f45b4af34eac47af00c1d85
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-13 16:11:32 +0530

    Update C.py

--- codeforces/virtual/984/C.py
+++ codeforces/virtual/984/C.py
@@ -22,40 +22,47 @@ sbhatore
 # pseudocode:
 """
 
-def calcAnswer(s, n, b, i, FLAG):
+def calcAnswer(s, n, b, i, count, FLAG):
     if (FLAG):
         for j in range (i - 3, i + 1):
-            if (j in b):
-                b.remove(j)
+            if (j >= 0):
+                if (b[j]):
+                    b[j] = False
+                    count -= 1
+                #
             #
         #
     #
     if ((i + 3) < n):
         if (s[i:i + 4] == "1100"):
-            if i not in b:
-                b.append(i)
+            if not b[i]:
+                b[i] = True
+                count += 1
             #
         #
     if ((i - 1 >= 0) and (i + 2 < n)):
         if (s[i - 1:i + 3] == "1100"):
-            if (i - 1) not in b:
-                b.append(i - 1)
+            if not b[i - 1]:
+                b[i - 1] = True
+                count += 1
             #
         #
     if ((i - 2 >= 0) and (i + 1 < n)):
         if (s[i - 2:i + 2] == "1100"):
-            if ((i - 2) not in b):
-                b.append(i - 2)
+            if (not b[i - 2]):
+                b[i - 2] = True
+                count += 1
             #
         #
     if ((i - 3 >= 0) and (i < n)):
         if (s[i - 3:i + 1] == "1100"):
-            if ((i - 3) not in b):
-                b.append(i - 3)
+            if (not b[i - 3]):
+                b[i - 3] = True
+                count += 1
             #
         #
     #
-    return b
+    return b, count
 
 
 def __main__():
@@ -68,19 +75,25 @@ def __main__():
             x, v = map(int, input().split())
             a.append((x, v))
         #
-        b = []
         n = len(s)
+        b = [True] * n
+        count = 0
         for i in range(0, n - 3):
             if (s[i:i + 4] == "1100"):
-                b.append(i)
+                b[i] = True
+                count += 1
+            else:
+                b[i] = False
             #
         #
+        for i in range(n - 3, n):
+            b[i] = False
         for i in range(0, q):
             FLAG1 = (a[i][1] != s[a[i][0] - 1])
             if (FLAG1):
                 s = s[:a[i][0] - 1] + str(a[i][1]) + s[a[i][0]:]
-                b = calcAnswer(s, n, b, a[i][0] - 1, FLAG1)
-            if (len(b) > 0):
+                b, count = calcAnswer(s, n, b, a[i][0] - 1, count, FLAG1)
+            if (count > 0):
                 print("YES")
             else:
                 print("NO")
