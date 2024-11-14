706bdacca4b737d815253158416bc8636ec21685
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-14 18:19:17 +0530

    Update D.py

--- codeforces/970/D.py
+++ codeforces/970/D.py
@@ -29,7 +29,29 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        a = list(map(int, input().split()))
+        s = input()
+        for i in range(0, n):
+            count = 0
+            if (s[i] == '0' and a[i] == (i + 1)):
+                count += 1
+            else:
+                itr1 = i
+                itr2 = a[itr1] - 1
+                while (itr2 != itr1 and (a[itr2] != (itr2 + 1))):
+                    if (s[itr2] == '0'):
+                        count += 1
+                    itr2 = a[itr2] - 1
+                #
+                if (itr2 == itr1):
+                    if (s[itr1] == '0'):
+                        count += 1
+            print(count, end=" ")
+        #
+        print("")
+    #
+    return
 
 
 __main__()
