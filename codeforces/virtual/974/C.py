4b93ead7fe83464a0ba9e980c392706756695ac7
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-14 08:37:34 +0530

    Update C.py

--- codeforces/virtual/974/C.py
+++ codeforces/virtual/974/C.py
@@ -24,28 +24,35 @@ sbhatore
 
 import math
 
-def calcAnswer(a, n, i, numA, num1):
+def calcAnswer(a, n, a_i, numA, num1):
+    if (a_i < num1):
+        print("0")
+        return
     x = 1
-    while (num1 <= a[n // 2]):
-        num1 = (numA + x) / 2
+    while ((num1 / 2) <= a_i):
+        num1 = (numA + x) / n
         x *= 2
     x = x // 2
     y = x // 2
     itr1 = y
     itr2 = x
+    FLAG = True
     while (itr1 <= itr2):
         mid = itr1 + (itr2 - itr1) // 2
         num1 = (numA + mid) / n
         num2 = (numA + mid - 1) / n
-        if (num2 <= a[n // 2] and num1 > a[n // 2]):
+        if ((num2 / 2) <= a_i and (num1 / 2) > a_i):
             print(mid)
+            FLAG = False
             break
-        elif (num2 <= a[n // 2]):
-            itr2 = mid - 1
-        else:
+        elif ((num2 / 2) <= a_i):
             itr1 = mid + 1
+        else:
+            itr2 = mid - 1
         #
     #
+    if (FLAG):
+        print(itr1)
 
 
 def __main__():
@@ -59,11 +66,7 @@ def __main__():
             numA = sum(a)
             num1 = numA / n
             a = sorted(a)
-            if (n % 2 == 0):
-                calcAnswer(a, n, n // 2, numA, num1)
-            else:
-                calcAnswer(a, n, n // 2 + 1, numA, num1)
-            #
+            calcAnswer(a, n, a[n // 2], numA, num1)
         #
     #
     return
