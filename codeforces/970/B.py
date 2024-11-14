020b383841fbb73d60c82278540704f836215edb
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-14 16:42:50 +0530

    codeforces 970: practice B.py accepted

--- codeforces/970/B.py
+++ codeforces/970/B.py
@@ -29,7 +29,24 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        s = input()
+        num1 = int(n ** (0.5))
+        num2 = None
+        if (num1 < 3):
+            num2 = 0
+        else:
+            num2 = (num1 - 2) * (num1 - 2)
+        if (num1 * num1 == n):
+            if (s.count('0') == num2):
+                print("Yes")
+            else:
+                print("No")
+            #
+        else:
+            print("No")
+        #
+    return
 
 
 __main__()
