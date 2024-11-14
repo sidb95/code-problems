bf60d09c6e4843ea6e37c00739c34f96e45e617f
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-14 17:29:40 +0530

    codeforces 970: practice C.py accepted

--- codeforces/970/C.py
+++ codeforces/970/C.py
@@ -29,7 +29,35 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        l, r = map(int, input().split())
+        if (l == r):
+            print(1)
+            continue
+        i = 1
+        while (l + ((i * (i - 1)) // 2) <= r):
+            i *= 2
+        itr1 = i // 2
+        itr2 = i
+        FLAG = True
+        while (itr1 <= itr2):
+            mid = itr1 + (itr2 - itr1) // 2
+            num1 = l + (mid * (mid - 1)) // 2
+            num2 = l + ((mid + 1) * (mid)) // 2
+            if (num1 <= r and num2 > r):
+                print(mid)
+                FLAG = False
+                break
+            elif (num1 > r):
+                itr2 = mid
+            else:
+                itr1 = mid + 1
+            #
+        #
+        if (FLAG):
+            print(itr1)
+        #
+    #
+    return
 
 
 __main__()
