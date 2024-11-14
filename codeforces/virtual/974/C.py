9fb3506e1d151a57c1bd8efc60e7d08ce442a622
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-14 05:54:11 +0530

    Update C.py

--- codeforces/virtual/974/C.py
+++ codeforces/virtual/974/C.py
@@ -1,14 +1,14 @@
 """
 sbhatore
-3 Sep 2024
+13 Nov 2024
 
 # Assumptions:
 #
 #
 #
 # In:
-#
-#
+# 1. n
+# 2. a
 #
 #
 # Out:
@@ -22,6 +22,8 @@ sbhatore
 # pseudocode:
 """
 
+import math
+
 def calcAnswer():
     pass
 
@@ -29,7 +31,32 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        n = int(input())
+        a = list(map(int, input().split()))
+        if (n < 3):
+            print("-1")
+        else:
+            num1 = sum(a) / n
+            a = sorted(a)
+            if (n % 2 == 0):
+                if (a[n // 2] < num1):
+                    print("0")
+                else:
+                    x = math.ceil((n * (a[n // 2] - sum(a))))
+                    print(x)
+                #
+            else:
+                if (a[n // 2 + 1] < num1):
+                    print("0")
+                else:
+                    x = math.ceil((n * (a[n // 2 + 1] - sum(a))))
+                    print(x)
+                #
+            #
+        #
+    #
+    return
+
 
 
 __main__()
