b37e7a23d634af650d24cbcbc00ba007800f81c3
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-11-22 18:09:01 +0530

    Update B.py

--- codeforces/virtual/988/B.py
+++ codeforces/virtual/988/B.py
@@ -29,7 +29,30 @@ def calcAnswer():
 def __main__():
     T = int(input())
     for t in range(0, T):
-        pass
+        k = int(input())
+        b = list(map(int, input().split()))
+        a = set(b)
+        A = {}
+        for elmt in a:
+            A[elmt] = True
+        FLAG = True
+        for elmt in a:
+            if (elmt != 0):
+                if ((k - 2) % elmt == 0):
+                    if ((k - 2) // elmt == elmt):
+                        if (b.count(elmt) > 1):
+                            print(elmt, elmt)
+                            break
+                    else:
+                        if (((k - 2) // elmt) in A):
+                            print(elmt, (k - 2) // elmt)
+                            break
+                    #
+                #
+            #
+        #
+    #
+    return
 
 
 __main__()
